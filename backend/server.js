424377b59e11eaa7da108efa2766be0fe9542d15
TranMinhHieu <tranvanminhhieu06@gmail.com>
2026-05-29 00:45:08 +0700

    fix: move dotenv config to the top of server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
-require('dotenv').config();
 const db = require('./models/db');
 
 // Khởi tạo các Route
